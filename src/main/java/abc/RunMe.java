ddf5adcb9510d7640d4931cdb925bce577be4e1f
Mark Sto <mstoyukhin@gmail.com>
2019-05-17 02:26:54 +0300

    Add enum test

--- src/main/java/abc/RunMe.java
+++ src/main/java/abc/RunMe.java
@@ -3,8 +3,33 @@ package abc;
 import abc.res.*;
 
 public class RunMe {
+
   public static void main(String[] args) {
-    Test good = Test.create("id", 1.0, 1);
-    System.out.println(good.write().toJson());
+    test_JSON_Schema();
+
+    test_Enum_Null();
+    test_Enum_Init();
+    //test_Enum_JDK9();
   }
+
+  private static void test_JSON_Schema() {
+    Test test = Test.create("id", 1.0, 1);
+    System.out.println(test.write().toJson());
+  }
+
+  private static void test_Enum_Null() {
+    EnumTest enumTest = EnumTest.create(null);
+    System.out.println(enumTest.write().toJson());
+  }
+
+  private static void test_Enum_Init() {
+    EnumTest enumTest = EnumTest.create(EnumTest.ind.y);
+    System.out.println(enumTest.write().toJson());
+  }
+
+//  private static void test_Enum_JDK9() {
+//    UnicodeTest enumTest = UnicodeTest.create(UnicodeTest.ind._);
+//    System.out.println(enumTest.write().toJson());
+//  }
+
 }
